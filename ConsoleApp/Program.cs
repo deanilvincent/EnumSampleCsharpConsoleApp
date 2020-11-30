0d0630bbf5a914736bd99634ff767e7b42e14ea3
Mark Vincent <markdeanilvicente@gmail.com>
2020-11-30 16:56:24 +0800

    initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var status = TransactionStatus.ForApproval;
+            Console.WriteLine(status.ToString());
+        }
+    }
+}
---
+++ ConsoleApp/TransactionStatus.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp
+{
+    public enum TransactionStatus
+    {
+        [Display(Name = "Pending")]
+        Pending = 0,
+        [Display(Name = "In Progress")]
+        InProgress = 1,
+        [Display(Name = "Completed")]
+        Completed = 2,
+        [Display(Name = "Cancelled")]
+        Cancelled = 3,
+        [Display(Name = "For Approval")]
+        ForApproval = 4
+    }
+}
